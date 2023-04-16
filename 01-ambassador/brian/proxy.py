45b54224dfab81dc0d8a6a2c8bcb34cdfc843d89
brianliu <hong86323@gmail.com>
2023-04-16 13:55:18 +0800

    add telemetry data source

--- 01-ambassador/brian/proxy.py
+++ 01-ambassador/brian/proxy.py
@@ -1,8 +1,12 @@
+import logging
+import os
+from datetime import datetime
 from http import HTTPStatus
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
-from fastapi import FastAPI, Header, Response
+import httpx
+from fastapi import BackgroundTasks, FastAPI, Header, Response
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 
@@ -11,13 +15,47 @@ app = FastAPI()
 
 @app.get("/{path_to_server:path}")
 async def forward(
-    path_to_server: str, x_destination_host: str = Header(min_length=1)
+    path_to_server: str,
+    background_tasks: BackgroundTasks,
+    x_destination_host: str = Header(min_length=1),
 ) -> Response:
+    start = datetime.now()
     async with AsyncClient() as client:
         response = await client.get(
             f"http://{x_destination_host}/{path_to_server}",
         )
+    time_passed = (datetime.now() - start).total_seconds()
+    record_time_metrix(time_passed, x_destination_host, path_to_server)
     return Response(content=response.content)
+
+
+def record_time_metrix(
+    time_used_in_secs: float,
+    target: str,
+    path: str,
+):
+    params = {
+        "org": "brian",
+        "bucket": "api",
+        "precision": "s",
+    }
+    headers = {
+        "Authorization": f"Bearer {os.environ.get('INFLUXDB_CONNECTION_TOKEN')}",
+        "Content-Type": "text/plain; charset=utf-8",
+        "Accept": "application/json",
+    }
+    content = (
+        f"time,target={target},uri={path}"
+        f" time_elapsed={time_used_in_secs}"
+        f" {int(datetime.utcnow().timestamp())}"
+    )
+    response = httpx.post(
+        "http://influxdb:8086/api/v2/write",
+        params=params,
+        headers=headers,
+        data=content,
+    )
+    logging.critical(response.status_code)
 
 
 class TestForward(TestCase):
