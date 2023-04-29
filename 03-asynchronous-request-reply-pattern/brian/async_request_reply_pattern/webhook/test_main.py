3de7f59dfddf1e0af65269da304efffee29d7eb8
brianliu <hong86323@gmail.com>
2023-04-29 17:43:09 +0800

    add e2e testing (code generation)

--- 03-asynchronous-request-reply-pattern/brian/async_request_reply_pattern/webhook/test_main.py
+++ 03-asynchronous-request-reply-pattern/brian/async_request_reply_pattern/webhook/test_main.py
@@ -1,7 +1,8 @@
 import json
 import time
-from unittest import TestCase
+from unittest import TestCase, skipIf
 from unittest.mock import patch
+import os
 
 from fastapi.testclient import TestClient
 from linebot.exceptions import InvalidSignatureError
@@ -78,11 +79,16 @@ class TestHandleWebhook(TestCase):
 class TestHandleWebhookWithRealAzureQueue(TestCase):
     client = TestClient(main.app)
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUp(cls) -> None:
         try:
             az.queue_client.create_queue()
         except ResourceExistsError:
             pass
+        az.queue_client.clear_messages()
+
+    def tearDown(self) -> None:
+        az.queue_client.clear_messages()
 
     def test_push_message_to_queue(self):
         content = "a message"
@@ -93,3 +99,61 @@ class TestHandleWebhookWithRealAzureQueue(TestCase):
         queue_content = json.loads(message.content)["prompt"]
 
         self.assertEqual(queue_content, content)
+
+@skipIf(not os.environ.get("e2e_test"), "not e2e test")
+class TestImageGenerationWorkflow(TestCase):
+    client = TestClient(main.app)
+
+    @classmethod
+    def setUp(cls) -> None:
+        try:
+            az.queue_client.create_queue()
+        except ResourceExistsError:
+            pass
+        try:
+            az.container_client.create_container()
+        except ResourceExistsError:
+            pass
+        az.queue_client.clear_messages()
+
+    def test_send_message_to_generate_image(self):
+        with patch.object(
+            main.handler.parser.signature_validator,
+            "validate",
+            autospec=True
+        ), patch("uuid.uuid4") as fake_uuid4:
+            fake_uuid4.return_value = "uuid"
+            event = {
+                "destination": "xxxxxxxxxx",
+                "events": [
+                    {
+                        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                        "type": "message",
+                        "mode": "active",
+                        "timestamp": 1462629479859,
+                        "source": {
+                            "type": "group",
+                            "groupId": "Ca56f94637c...",
+                            "userId": "U4af4980629..."
+                        },
+                        "webhookEventId": "01FZ74A0TDDPYRVKNK77XKC3ZR",
+                        "deliveryContext": {
+                            "isRedelivery": False
+                        },
+                        "message": {
+                            "id": "444573844083572737",
+                            "type": "text",
+                            "text": "A polar bear doing bouldering.",
+                        }
+                    }
+                ]
+            }
+
+            self.client.post(
+                "/", headers={"X-Line_Signature": "valid"}, json=event
+            )
+            time.sleep(20)
+
+            blob = az.container_client.get_blob_client("uuid")
+            self.assertTrue(blob.exists())
+            self.assertTrue(blob.download_blob().readall().startswith("https"))
